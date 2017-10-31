6137c4dc1a17fa04f21d8fced634ea7c84272818
Staś Małolepszy <stas@mozilla.com>
2017-10-31 13:59:39 +0100

    Bug 1317336 - Part 1 - Add tests for %S, %1$S etc.

--- tests/migrate/test_replace.py
+++ tests/migrate/test_replace.py
@@ -20,7 +20,7 @@ class MockContext(unittest.TestCase):
 
 
 @unittest.skipUnless(PropertiesParser, 'compare-locales required')
-class TestReplace(MockContext):
+class TestReplaceExplicit(MockContext):
     def setUp(self):
         self.strings = parse(PropertiesParser, '''
             hello = Hello, #1!
@@ -143,6 +143,177 @@ class TestReplace(MockContext):
             evaluate(self, msg).to_json(),
             ftl_message_to_json('''
                 last = Foo { $bar }
+            ''')
+        )
+
+
+@unittest.skipUnless(PropertiesParser, 'compare-locales required')
+class TestReplaceStringBundle(MockContext):
+    def setUp(self):
+        self.strings = parse(PropertiesParser, '''
+            implicit = %S %S
+            explicit = %2$S %1$S
+            zero-width-implicit = %S%0.S
+            zero-width-explicit = %2$S%1$0.S
+        ''')
+
+    def test_replace_implicit(self):
+        msg = FTL.Message(
+            FTL.Identifier('implicit'),
+            value=REPLACE(
+                self.strings,
+                'implicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two')
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                implicit = { $one } { $two }
+            ''')
+        )
+
+    def test_replace_implicit_too_many(self):
+        msg = FTL.Message(
+            FTL.Identifier('implicit'),
+            value=REPLACE(
+                self.strings,
+                'implicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two'),
+                    EXTERNAL_ARGUMENT('three'),
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                implicit = { $one } { $two }
+            ''')
+        )
+
+    def test_replace_implicit_too_few(self):
+        msg = FTL.Message(
+            FTL.Identifier('implicit'),
+            value=REPLACE(
+                self.strings,
+                'implicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                implicit = { $one } %S
+            ''')
+        )
+
+    def test_replace_explicit(self):
+        msg = FTL.Message(
+            FTL.Identifier('explicit'),
+            value=REPLACE(
+                self.strings,
+                'explicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two')
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                explicit = { $two } { $one }
+            ''')
+        )
+
+    def test_replace_explicit_too_many(self):
+        msg = FTL.Message(
+            FTL.Identifier('explicit'),
+            value=REPLACE(
+                self.strings,
+                'explicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two'),
+                    EXTERNAL_ARGUMENT('three'),
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                explicit = { $two } { $one }
+            ''')
+        )
+
+    def test_replace_explicit_too_few(self):
+        msg = FTL.Message(
+            FTL.Identifier('explicit'),
+            value=REPLACE(
+                self.strings,
+                'explicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                explicit = %2$S { $one }
+            ''')
+        )
+
+    def test_replace_zero_width_implicit(self):
+        msg = FTL.Message(
+            FTL.Identifier('zero-width-implicit'),
+            value=REPLACE(
+                self.strings,
+                'zero-width-implicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two')
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                zero-width-implicit = { $one }
+            ''')
+        )
+
+    def test_replace_zero_width_explicit(self):
+        msg = FTL.Message(
+            FTL.Identifier('zero-width-explicit'),
+            value=REPLACE(
+                self.strings,
+                'zero-width-explicit',
+                (
+                    EXTERNAL_ARGUMENT('one'),
+                    EXTERNAL_ARGUMENT('two')
+                )
+            )
+        )
+
+        self.assertEqual(
+            evaluate(self, msg).to_json(),
+            ftl_message_to_json('''
+                zero-width-explicit = { $two }
             ''')
         )
 
